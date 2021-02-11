5abb4add7294b6fc1ee0672e52376a8a4b9fd57c
chrisgallivan <41701655+chrisgallivan@users.noreply.github.com>
2021-02-11 15:54:38 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,32 +1,32 @@
 #terraform remote state
 terraform {
-     required_providers {
-        azurerm = {
-           source = "hashicorp/azurerm"
-        }
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
     }
-    backend "remote" {
-	organization = "KATA-FRIDAYS"
-	workspaces {
-		name = "kata-friday-test"
-	}
-   }
+  }
+  backend "remote" {
+    organization = "KATA-FRIDAYS"
+    workspaces {
+      name = "kata-friday-test"
+    }
+  }
 }
-variable "CLIENT_ID"{
+variable "CLIENT_ID" {
 }
-variable "CLIENT_SECRET"{
+variable "CLIENT_SECRET" {
 }
-variable "SUBSCRIPTION_ID"{
+variable "SUBSCRIPTION_ID" {
 }
-variable "TENANT_ID"{
+variable "TENANT_ID" {
 }
 
 provider "azurerm" {
-   client_id = var.CLIENT_ID
-   client_secret = var.CLIENT_SECRET
-   subscription_id= var.SUBSCRIPTION_ID
-   tenant_id= var.TENANT_ID
-   features {}
+  client_id       = var.CLIENT_ID
+  client_secret   = var.CLIENT_SECRET
+  subscription_id = var.SUBSCRIPTION_ID
+  tenant_id       = var.TENANT_ID
+  features {}
 }
 
 resource "azurerm_resource_group" "example" {
